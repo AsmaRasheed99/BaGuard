a538a93dff00321c37f18aef6390886486316455
AsmaRasheed99 <AsmaRasheed4599@gmail.com>
2023-08-24 12:35:45 +0300

    edit path

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT ;
 const dbURI = process.env.dbURI;
 const userRoute = require('./routes/userRoute')
 const contactRoute = require('./routes/contactRoute')
-
 const mongoose = require("mongoose");
 const path = require("path");
 
@@ -14,14 +13,14 @@ app.use(cors());
 app.use(express.json());
 
 // to redirect frontend
-app.use(express.static(path.join(__dirname, "../Client/dist")));
+app.use(express.static(path.join(__dirname, "/../Client/dist")));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "../Client/dist/index.html"));
+  res.sendFile(path.join(__dirname + "/../Client/dist/index.html"));
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Welcome");
-// });
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 
 app.use(userRoute)
 app.use(contactRoute)

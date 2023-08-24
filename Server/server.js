ba16a8815f9c85dde53db8108980451184830a20
AsmaRasheed99 <AsmaRasheed4599@gmail.com>
2023-08-24 12:02:09 +0300

    edit port

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 6000;
 const dbURI = process.env.dbURI;
 const userRoute = require('./routes/userRoute')
 const contactRoute = require('./routes/contactRoute')

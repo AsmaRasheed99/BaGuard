9e74c70b693820b911f01dac1aa2cb267a8edd9b
Asma Rasheed <Asmarasheed4599@gmail.com>
2023-09-04 23:07:00 +0300

    redirect

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const httpProxy = require("http-proxy"); // Import http-proxy
 const PORT = process.env.PORT;
 const dbURI = process.env.dbURI;
 const userRoute = require('./routes/userRoute');
@@ -12,8 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Create a proxy instance
-const proxy = httpProxy.createProxyServer({});
 
 // Serve the client application's static files
 app.use(express.static(path.join(__dirname, '../Client', 'dist')));
@@ -25,11 +22,7 @@ app.get('/', (req, res) => {
 app.use(userRoute);
 app.use(contactRoute);
 
-// Proxy route
-app.get("/proxy", (req, res) => {
-  // Proxy the request to the external resource
-  proxy.web(req, res, { target: 'http://external-domain.com' }); // Replace with the actual external domain
-});
+
 
 // Start the server
 module.exports = {
